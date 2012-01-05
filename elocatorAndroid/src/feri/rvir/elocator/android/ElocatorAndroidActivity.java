55195d8b1e35b2a759b02c2db7cc040d8f8b93a1
gpanic <gregor.panic@gmail.com>
2012-01-05 11:13:31 +0100

    Dodan android projekt.

---
+++ elocatorAndroid/src/feri/rvir/elocator/android/ElocatorAndroidActivity.java
@@ -0,0 +1,13 @@
+package feri.rvir.elocator.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ElocatorAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
