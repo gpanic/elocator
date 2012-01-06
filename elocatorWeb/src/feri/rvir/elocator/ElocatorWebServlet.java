a5b99e8c3e984f7eec92161c2becd9b482a43cc4
gregor.panic <gregor.panic@gmail.com>
2012-01-06 08:08:21 +0100

    ...

---
+++ elocatorWeb/src/feri/rvir/elocator/ElocatorWebServlet.java
@@ -0,0 +1,13 @@
+package feri.rvir.elocator;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class ElocatorWebServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
