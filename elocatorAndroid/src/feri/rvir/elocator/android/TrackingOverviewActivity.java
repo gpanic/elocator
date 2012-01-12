9afd26f320c027a7024160c02a7c6bc7974f4a73
Gregor Panic <gregor.panic@gmail.com>
2012-01-12 10:37:22 +0100

    Dodal Google Maps.

--- elocatorAndroid/src/feri/rvir/elocator/android/TrackingOverviewActivity.java
+++ elocatorAndroid/src/feri/rvir/elocator/android/TrackingOverviewActivity.java
@@ -1,17 +1,43 @@
 package feri.rvir.elocator.android;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.TextView;
+import java.util.List;
 
-public class TrackingOverviewActivity extends Activity {
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+
+import feri.rvir.elocator.android.maps.TrackingItemizedOverlay;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+public class TrackingOverviewActivity extends MapActivity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		TextView tv=new TextView(this);
-		tv.setText("Tracking Overview Tab");
-		setContentView(tv);
+		setContentView(R.layout.trackingoverview);
+		
+		MapView mapView=(MapView)findViewById(R.id.trackingoverview_mapview);
+		mapView.setBuiltInZoomControls(true);
+		
+		List<Overlay> mapOverlays = mapView.getOverlays();
+		Drawable drawable = this.getResources().getDrawable(R.drawable.ic_marker);
+		TrackingItemizedOverlay itemizedoverlay = new TrackingItemizedOverlay(drawable);
+		
+		GeoPoint point = new GeoPoint(19240000,-99120000);
+		OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
+		
+		itemizedoverlay.addOverlay(overlayitem);
+		mapOverlays.add(itemizedoverlay);
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
