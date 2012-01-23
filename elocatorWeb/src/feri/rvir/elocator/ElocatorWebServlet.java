a45130ef0aaa82a19a07ffbe946dadf1a2da481d
mantic0 <jernej.legvart@gmail.com>
2012-01-23 21:06:52 +0100

    ElocatorWebServlet fixed

--- elocatorWeb/src/feri/rvir/elocator/ElocatorWebServlet.java
+++ elocatorWeb/src/feri/rvir/elocator/ElocatorWebServlet.java
@@ -42,16 +42,6 @@ public class ElocatorWebServlet extends HttpServlet {
 		LocationDao ldao = new LocationDao();
 		//ldao.addLocation(l);
 		
-		/*
-		UserDao udao=new UserDao();
-		LocationDao ldao=new LocationDao();
-		TrackingDao tdao=new TrackingDao();
-		System.out.println(udao.getAll().size());
-		System.out.println(ldao.getAll().size());
-		System.out.println(tdao.getAllTrackings().size());
-		
-		System.out.println(udao.getAll().get(0).getUsername());
-		System.out.println(udao.getAll().get(0).getPassword());
-		System.out.println(Crypto.hash("test", "SHA-1"));
+	
 	}
 }
