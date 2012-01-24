22fe58ec10c7eb7eda76d7d9c56ac1940ebd8132
Gregor Panic <gregor.panic@gmail.com>
2012-01-24 11:31:19 +0100

    mojstersko delo...

--- elocatorAndroid/gen/feri/rvir/elocator/android/R.java
+++ elocatorAndroid/gen/feri/rvir/elocator/android/R.java
@@ -18,39 +18,41 @@ public final class R {
         public static final int ic_marker_info=0x7f020004;
         public static final int ic_marker_start=0x7f020005;
         public static final int ic_marker_user=0x7f020006;
-        public static final int ic_tab_preferences=0x7f020007;
-        public static final int ic_tab_preferences_selected=0x7f020008;
-        public static final int ic_tab_preferences_unselected=0x7f020009;
-        public static final int ic_tab_tracking_details=0x7f02000a;
-        public static final int ic_tab_tracking_details_selected=0x7f02000b;
-        public static final int ic_tab_tracking_details_unselected=0x7f02000c;
-        public static final int ic_tab_tracking_overview=0x7f02000d;
-        public static final int ic_tab_tracking_overview_selected=0x7f02000e;
-        public static final int ic_tab_tracking_overview_unselected=0x7f02000f;
+        public static final int ic_menu_sync=0x7f020007;
+        public static final int ic_tab_preferences=0x7f020008;
+        public static final int ic_tab_preferences_selected=0x7f020009;
+        public static final int ic_tab_preferences_unselected=0x7f02000a;
+        public static final int ic_tab_tracking_details=0x7f02000b;
+        public static final int ic_tab_tracking_details_selected=0x7f02000c;
+        public static final int ic_tab_tracking_details_unselected=0x7f02000d;
+        public static final int ic_tab_tracking_overview=0x7f02000e;
+        public static final int ic_tab_tracking_overview_selected=0x7f02000f;
+        public static final int ic_tab_tracking_overview_unselected=0x7f020010;
     }
     public static final class id {
-        public static final int details_mapview=0x7f060014;
-        public static final int details_spinnerUsers=0x7f060013;
-        public static final int details_textViewYouAreTracking=0x7f060012;
-        public static final int linearLayout1=0x7f060009;
-        public static final int main_buttonSignIn=0x7f060007;
-        public static final int main_editTextPassword=0x7f060006;
-        public static final int main_editTextUsername=0x7f060004;
-        public static final int main_linearLayout1=0x7f060000;
-        public static final int main_linearLayout2=0x7f060001;
-        public static final int main_textViewLargeSignIn=0x7f060002;
-        public static final int main_textViewPassword=0x7f060005;
-        public static final int main_textViewRegister=0x7f060008;
-        public static final int main_textViewUsername=0x7f060003;
-        public static final int reg_buttonRegister=0x7f060011;
-        public static final int reg_editTextPassword=0x7f06000e;
-        public static final int reg_editTextPasswordRepeat=0x7f060010;
-        public static final int reg_editTextUsername=0x7f06000c;
-        public static final int reg_textViewLargeReg=0x7f06000a;
-        public static final int reg_textViewPassword=0x7f06000d;
-        public static final int reg_textViewPasswordRepeat=0x7f06000f;
-        public static final int reg_textViewUsername=0x7f06000b;
-        public static final int trackingoverview_mapview=0x7f060015;
+        public static final int details_mapview=0x7f070014;
+        public static final int details_spinnerUsers=0x7f070013;
+        public static final int details_textViewYouAreTracking=0x7f070012;
+        public static final int linearLayout1=0x7f070009;
+        public static final int main_buttonSignIn=0x7f070007;
+        public static final int main_editTextPassword=0x7f070006;
+        public static final int main_editTextUsername=0x7f070004;
+        public static final int main_linearLayout1=0x7f070000;
+        public static final int main_linearLayout2=0x7f070001;
+        public static final int main_textViewLargeSignIn=0x7f070002;
+        public static final int main_textViewPassword=0x7f070005;
+        public static final int main_textViewRegister=0x7f070008;
+        public static final int main_textViewUsername=0x7f070003;
+        public static final int options_trackingoverview_sync=0x7f070016;
+        public static final int reg_buttonRegister=0x7f070011;
+        public static final int reg_editTextPassword=0x7f07000e;
+        public static final int reg_editTextPasswordRepeat=0x7f070010;
+        public static final int reg_editTextUsername=0x7f07000c;
+        public static final int reg_textViewLargeReg=0x7f07000a;
+        public static final int reg_textViewPassword=0x7f07000d;
+        public static final int reg_textViewPasswordRepeat=0x7f07000f;
+        public static final int reg_textViewUsername=0x7f07000b;
+        public static final int trackingoverview_mapview=0x7f070015;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -58,6 +60,9 @@ public final class R {
         public static final int registration=0x7f030002;
         public static final int trackingdetails=0x7f030003;
         public static final int trackingoverview=0x7f030004;
+    }
+    public static final class menu {
+        public static final int trackingoverview_options_menu=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
