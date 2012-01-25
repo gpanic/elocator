eeeeaaeb8aecc5fc05def99d0f336a03e0157b11
mantic0 <jernej.legvart@gmail.com>
2012-01-25 10:25:36 +0100

    Test

--- elocatorWeb/src/feri/rvir/elocator/ElocatorWebServlet.java
+++ elocatorWeb/src/feri/rvir/elocator/ElocatorWebServlet.java
@@ -1,12 +1,31 @@
 package feri.rvir.elocator;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
 
 import javax.servlet.http.*;
+
+import feri.rvir.elocator.dao.LocationDao;
+import feri.rvir.elocator.dao.UserDao;
+import feri.rvir.elocator.rest.resource.location.Location;
+import feri.rvir.elocator.rest.resource.user.User;
 
 @SuppressWarnings("serial")
 public class ElocatorWebServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+		
+		UserDao udao = new UserDao();
+		User u = new User();
+		u = udao.getUser("test");
+		System.out.println(u.getKey() + " je key");
+		
+		Calendar cal = Calendar.getInstance();
+		Date now = cal.getTime();
+		Location l = new Location(u.getKey(),now,46.565617,15.62346);
+		LocationDao ldao = new LocationDao();
+		
+		ldao.addLocation(l);
 	}
 }
